f1c8e7ac80acc9f08961f62099ad967fbb945549
unknown <oanhtt@hblab/vn>
2026-01-08 13:53:21 +0700

    add build

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))  # if you need to import your package
 
-project = 'Benchmark Tool'
+project = 'LLM Benchmark Tool'
 author = 'Trương Thị Oanh'
 release = '0.1.0'
 
@@ -18,7 +18,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'alabaster'  # or change to 'sphinx_rtd_theme' if installed
+html_theme = 'classic'  # or change to 'sphinx_rtd_theme' if installed
 html_static_path = ['_static']
 language = 'vi'
 html_css_files = ['custom.css']
